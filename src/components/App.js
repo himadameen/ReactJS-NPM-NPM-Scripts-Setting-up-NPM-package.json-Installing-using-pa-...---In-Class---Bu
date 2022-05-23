24f2dceef3e9dec8ed66923bf2a64eb9291c890e
Subhradip Nath <97626209+SUVRO96@users.noreply.github.com>
2022-05-23 19:07:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
         Click
       </button>
       {show && (
-        <p id="parah">
+        <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy
         </p>
