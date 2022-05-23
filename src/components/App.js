c59c0d40bc1897169bdb9e3ef45d1ab3727161c1
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-05-23 18:27:10 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [show, setShow] = useState(false);
+
+  const parah = () => {
+    setShow(true);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={parah}>
+        Click
+      </button>
+      {show && (
+        <p id="parah">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
-
 
 export default App;
